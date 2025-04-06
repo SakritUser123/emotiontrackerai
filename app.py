c12c547b3d6750d99995ff977d3d5c99bf158e28
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-04-06 18:47:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ page = st.sidebar.radio("Go to:", ("Home", "Multi-Emotions"))
 
 # Switch between pages based on sidebar choice
 if page == "Home":
-    import home  # This imports the main.py script
+    import main  # This imports the main.py script
 elif page == "Multi-Emotions":
     import multiemotions  # This imports the multiemotions.py script
